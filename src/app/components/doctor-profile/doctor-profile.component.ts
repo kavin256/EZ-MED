53be2b304f9d0b9dad7577e152e17cbf7f5eec36
sandaruwanims <sandaruwanims@gmail.com>
2021-01-17 22:06:36 +0530

    Patient Doctor profile image upload and display development

--- src/app/components/doctor-profile/doctor-profile.component.ts
+++ src/app/components/doctor-profile/doctor-profile.component.ts
@@ -3,150 +3,151 @@ import {Router} from '@angular/router';
 import {DataLoaderService} from '../../services/data-loader.service';
 import {Constants, currencyCodes, DoctorTitles, DoctorType, specializations} from '../../utils/Constants';
 import {DataKey, DataStoreService, LocalStorageKeys} from '../../services/data-store.service';
-import {HttpHeaders, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams, HttpRequest} from '@angular/common/http';
 import {DoctorSpecificData, UserData} from '../../models/user-data';
 import {DataHandlerService} from '../../services/data-handler.service';
 
 @Component({
-  selector: 'app-doctor-profile',
-  templateUrl: './doctor-profile.component.html',
-  styleUrls: ['./doctor-profile.component.css']
+    selector: 'app-doctor-profile',
+    templateUrl: './doctor-profile.component.html',
+    styleUrls: ['./doctor-profile.component.css']
 })
 export class DoctorProfileComponent implements OnInit {
-  selectedImage: File;
-  // profileUsername = 'dfg';
-  editable = false;
-  loggedInUser = null;
-  priceCurrency = 'LKR';
-  userData: DoctorSpecificData;
+    selectedImage: File;
+    // profileUsername = 'dfg';
+    editable = false;
+    loggedInUser = null;
+    priceCurrency = 'LKR';
+    userData: DoctorSpecificData;
 
-  titles = [
-    {value: DoctorTitles.DR},
-    {value: DoctorTitles.MR},
-    {value: DoctorTitles.MRS},
-    {value: DoctorTitles.MS},
-    {value: DoctorTitles.PROF},
-  ];
+    titles = [
+        {value: DoctorTitles.DR},
+        {value: DoctorTitles.MR},
+        {value: DoctorTitles.MRS},
+        {value: DoctorTitles.MS},
+        {value: DoctorTitles.PROF},
+    ];
 
-  doctorTypes = [
-    {value: DoctorType.CON},
-    {value: DoctorType.GEN},
-    {value: DoctorType.OTH}
-  ];
+    doctorTypes = [
+        {value: DoctorType.CON},
+        {value: DoctorType.GEN},
+        {value: DoctorType.OTH}
+    ];
 
-  // todo: find a better solution. this is just a duplication. So not good
-  specializations = specializations;
+    // todo: find a better solution. this is just a duplication. So not good
+    specializations = specializations;
 
-  constructor(
-      private router: Router,
-      private dataStore: DataStoreService,
-      private dataHandlerService: DataHandlerService,
-      private dataLoaderService: DataLoaderService
-  ) { }
-
-  ngOnInit() {
-    this.loggedInUser = JSON.parse(localStorage.getItem(LocalStorageKeys.loggedInUser));
-    // todo: resolve this commented
-    // if (this.loggedInUser && this.loggedInUser.doctorData) {
-    //   this.userData = this.loggedInUser.doctorData;
-    // }
-
-    // if not logged In this page should not be able to access
-    this.dataHandlerService.redirectToSignUpIfNotLoggedIn(JSON.parse(localStorage.getItem(LocalStorageKeys.loggedInUser)));
-    if (this.loggedInUser && this.loggedInUser) {
-      this.userData = this.loggedInUser;
+    constructor(
+        private router: Router,
+        private dataStore: DataStoreService,
+        private dataHandlerService: DataHandlerService,
+        private dataLoaderService: DataLoaderService,
+        private https: HttpClient
+    ) {
     }
 
-    // converting professionalType to a user friendly readable format
-    if (this.userData && this.userData.professionalType) {
-      this.userData.professionalType = this.dataHandlerService.convertProfessionalTypeFromDBFormat(
-          JSON.parse(JSON.stringify(this.userData.professionalType)));
+    ngOnInit() {
+        this.loggedInUser = JSON.parse(localStorage.getItem(LocalStorageKeys.loggedInUser));
+        // todo: resolve this commented
+        // if (this.loggedInUser && this.loggedInUser.doctorData) {
+        //   this.userData = this.loggedInUser.doctorData;
+        // }
+
+        // if not logged In this page should not be able to access
+        this.dataHandlerService.redirectToSignUpIfNotLoggedIn(JSON.parse(localStorage.getItem(LocalStorageKeys.loggedInUser)));
+        if (this.loggedInUser && this.loggedInUser) {
+            this.userData = this.loggedInUser;
+        }
+
+        // converting professionalType to a user friendly readable format
+        if (this.userData && this.userData.professionalType) {
+            this.userData.professionalType = this.dataHandlerService.convertProfessionalTypeFromDBFormat(
+                JSON.parse(JSON.stringify(this.userData.professionalType)));
+        }
     }
-  }
 
-  getColor(state: string) {
-    return '#000000';
-  }
+    getColor(state: string) {
+        return '#000000';
+    }
 
-  toggleEditable(editable: boolean) {
-    this.editable = editable;
-  }
+    toggleEditable(editable: boolean) {
+        this.editable = editable;
+    }
 
-  saveData() {
-    if (this.userData.professionalType &&
-        this.userData.priceForAppointment &&
-        parseInt(this.userData.priceForAppointment, 10) > 0 &&
-        this.userData.professionalType) {
-      // converting professionalType to a database readable format
-      if (this.userData && this.userData.professionalType) {
-        this.userData.professionalType = this.dataHandlerService.convertProfessionalTypeToDBFormat(
-            JSON.parse(JSON.stringify(this.userData.professionalType)));
-      }
-      // todo: change this in the backend
-      this.userData.doctorType = this.userData.professionalType;
-      const url = Constants.BASE_URL + Constants.UPDATE_PROFESSIONAL_SPECIFIC_DATA + this.userData.userName;
-      this.dataLoaderService.put<UserData>(url, new HttpParams(), new HttpHeaders(), DataKey.uploadImage, this.userData )
-          .then((data: any) => {
-            if (data && data.status && data.status.code === 1) {
-              // console.log('data');
-              // console.log(data.data);
-              // todo: check the following data[0]
-              // todo: data format from BE should be updated / changed
-              localStorage.setItem(LocalStorageKeys.loggedInUser, JSON.stringify(data.data[0]));
-              this.toggleEditable(false);
-
-            } else if (data && data.status && data.status.code === -1) {
-              // console.log('data null');
-              // console.log(data.data);
+    saveData() {
+        if (this.userData.professionalType &&
+            this.userData.priceForAppointment &&
+            parseInt(this.userData.priceForAppointment, 10) > 0 &&
+            this.userData.professionalType) {
+            // converting professionalType to a database readable format
+            if (this.userData && this.userData.professionalType) {
+                this.userData.professionalType = this.dataHandlerService.convertProfessionalTypeToDBFormat(
+                    JSON.parse(JSON.stringify(this.userData.professionalType)));
             }
-          });
-    } else {
-      // Todo: show a proper error
-      alert('Please fill mandatory fields. Price per consultation should be more than LKR 0');
+            // todo: change this in the backend
+            this.userData.doctorType = this.userData.professionalType;
+            const url = Constants.BASE_URL + Constants.UPDATE_PROFESSIONAL_SPECIFIC_DATA + this.userData.userName;
+            this.dataLoaderService.put<UserData>(url, new HttpParams(), new HttpHeaders(), DataKey.uploadImage, this.userData)
+                .then((data: any) => {
+                    if (data && data.status && data.status.code === 1) {
+                        // console.log('data');
+                        // console.log(data.data);
+                        // todo: check the following data[0]
+                        // todo: data format from BE should be updated / changed
+                        localStorage.setItem(LocalStorageKeys.loggedInUser, JSON.stringify(data.data[0]));
+                        this.toggleEditable(false);
+
+                    } else if (data && data.status && data.status.code === -1) {
+                        // console.log('data null');
+                        // console.log(data.data);
+                    }
+                });
+        } else {
+            // Todo: show a proper error
+            alert('Please fill mandatory fields. Price per consultation should be more than LKR 0');
+        }
     }
-  }
 
-  isConsultant(type: string) {
-    return type === DoctorType.CON;
-  }
+    isConsultant(type: string) {
+        return type === DoctorType.CON;
+    }
 
-  goToMyAppointments() {
-    this.router.navigate(['appointments']).then(r => {
-    });
-  }
-
-  editSchedule() {
-    this.router.navigate(['doctor/schedule']).then(r => {
-    });
-  }
-
-  uploadImage(event) {
-    this.selectedImage = event.target.file;
-    const formData = new FormData();
-    formData.append('image', this.selectedImage);
-    formData.append( 'username', this.userData.userName);
-
-    // sent request
-    const url = Constants.BASE_URL + Constants.UPLOAD_USER_IMAGE;
-    this.dataLoaderService.post<UserData>(url, new HttpParams(), new HttpHeaders(), DataKey.uploadImage, formData )
-        .then((data: any) => {
-          if (data && data.status && data.status.code === 1) {
-            // console.log('data');
-            // console.log(data.data);
-          } else if (data && data.status && data.status.code === -1) {
-            // console.log('data null');
-            // console.log(data.data);
-          }
+    goToMyAppointments() {
+        this.router.navigate(['appointments']).then(r => {
         });
-  }
+    }
 
-  checkForMandatoryFieldsToActivateProfile(userData: DoctorSpecificData) {
-    // currently only the userData.priceForAppointment is checked as a requirement
-    return userData &&
-        userData.priceForAppointment !== null &&
-        userData.priceForAppointment !== undefined &&
-        userData.priceForAppointment !== '' &&
-        parseInt(userData.priceForAppointment, 10) &&
-        parseInt(userData.priceForAppointment, 10) > 0;
-  }
+    editSchedule() {
+        this.router.navigate(['doctor/schedule']).then(r => {
+        });
+    }
+
+    uploadImage(event) {
+        this.selectedImage = event.target.files[0];
+        const formData: FormData = new FormData();
+        formData.append('file', this.selectedImage);
+
+        // sent request
+        const url = Constants.BASE_URL + Constants.UPLOAD_USER_IMAGE + this.userData.userName;
+        const req = new HttpRequest('POST', url, formData, {
+            reportProgress: true,
+            responseType: 'json'
+        });
+        this.https.request(req).subscribe(
+            data => {
+                if (data) {
+                }
+            }
+        );
+    }
+
+    checkForMandatoryFieldsToActivateProfile(userData: DoctorSpecificData) {
+        // currently only the userData.priceForAppointment is checked as a requirement
+        return userData &&
+            userData.priceForAppointment !== null &&
+            userData.priceForAppointment !== undefined &&
+            userData.priceForAppointment !== '' &&
+            parseInt(userData.priceForAppointment, 10) &&
+            parseInt(userData.priceForAppointment, 10) > 0;
+    }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,9 @@ import './header.component.css';
 import {Router} from '@angular/router';
 import {DataKey, DataStoreService, LocalStorageKeys} from '../../services/data-store.service';
 import {DataHandlerService} from '../../services/data-handler.service';
+import {Constants} from '../../utils/Constants';
+import {HttpClient, HttpRequest} from '@angular/common/http';
+import {DataLoaderService} from '../../services/data-loader.service';
 
 @Component({
   selector: 'app-header',
@@ -11,6 +14,8 @@ import {DataHandlerService} from '../../services/data-handler.service';
 })
 
 export class HeaderComponent implements OnInit, AfterViewInit {
+
+  profileImageURL = Constants.BASE_URL + Constants.DOWNLOAD_USER_IMAGE;
 
   signUpResultObject = {
     isSignUp: undefined,
@@ -28,7 +33,9 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   constructor(
               private dataHandlerService: DataHandlerService,
               private router: Router,
-              private dataStore: DataStoreService
+              private dataStore: DataStoreService,
+              private dataLoaderService: DataLoaderService,
+              private https: HttpClient
   ) {}
 
   ngOnInit() {
@@ -44,10 +51,34 @@ export class HeaderComponent implements OnInit, AfterViewInit {
       this.userType = this.signUpResultObject.userType;
       this.isSignUp = this.signUpResultObject.isSignUp;
     }
+    this.setProfileImageSource();
   }
 
   ngAfterViewInit() {
     setTimeout(() => { window.scroll(0, 0); }, 1000);
+  }
+
+  /**
+   * Set profile image source. If there is no image, set default avatar
+   */
+  setProfileImageSource() {
+    if (this.loggedInUser) {
+      this.profileImageURL += this.loggedInUser.userName;
+      // get image and verify that it is in the storage
+      const req = new HttpRequest('GET', this.profileImageURL, {
+        reportProgress: true
+      });
+      this.https.request(req).subscribe(
+          data => {},
+          error => {
+            if (error.status !== 200) {
+              this.profileImageURL = './assets/img/profile_blue1.png';
+            }
+          }
+      );
+    } else {
+      this.profileImageURL = './assets/img/profile_blue1.png';
+    }
   }
 
   logoClick(): void {
