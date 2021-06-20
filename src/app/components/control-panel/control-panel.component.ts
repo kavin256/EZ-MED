3a59c384d49dc894b38ec9a379b4e9e72c26912f
kavinranawella <kavin256@gmail.com>
2021-06-20 21:40:06 +0530

    Enhancement: setting the add config call

--- src/app/components/control-panel/control-panel.component.ts
+++ src/app/components/control-panel/control-panel.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {Prescription} from '../../models/prescription';
+import {HttpHeaders, HttpParams} from '@angular/common/http';
+import {ConfigModel} from '../../models/config';
+import {Constants} from '../../utils/Constants';
+import {DataLoaderService} from '../../services/data-loader.service';
 
 @Component({
     selector: 'app-control-panel',
@@ -7,11 +12,33 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ControlPanelComponent implements OnInit {
     visibleSection = '';
+    code = '';
+    desc = '';
+    configValue = '';
 
-    constructor() {
+    constructor(
+        public dataLoaderService: DataLoaderService
+    ) {
     }
 
     ngOnInit() {
     }
 
+    addConfig() {
+        const configModel = new ConfigModel();
+        configModel.name = this.code;
+        configModel.description = this.desc;
+        configModel.value = this.configValue;
+
+        // create url and send request
+        const url = Constants.API_BASE_URL + Constants.LOAD_CONFIGURATIONS;
+        this.dataLoaderService.post<Prescription>(url, new HttpParams(), new HttpHeaders(), null, configModel)
+            .then((data: any) => {
+                if (data && data.status && data.status.code === 1 && data.data && data.data.length > 0) {
+                    console.log();
+                } else if (data && data.status && data.status.code === -1) {
+                    console.log(data);
+                }
+            });
+    }
 }
